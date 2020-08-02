53f87f8a4a752d52767f9c07e770869ff9c66538
tomwaks <tom.waks.21@gmail.com>
2020-08-02 15:52:10 +0200

    added searching

--- app/src/main/java/com/tomwaks/psdapp/MainActivity.java
+++ app/src/main/java/com/tomwaks/psdapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.tomwaks.psdapp;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,29 +8,22 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -38,6 +31,8 @@ import org.json.JSONObject;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -49,14 +44,17 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawer_layout;
-    LinearLayout ll_main, ll_global;
-    ProgressBar pb_global;
-    TextView tv_search, tv_root;
-    ImageView iv_refresh, iv_close;
+    LinearLayout ll_main, ll_global, ll_no_objects;
+    ProgressBar pb_global, pb_storage;
+    TextView tv_search, tv_root, tv_storage, tv_info;
+    ImageView iv_refresh, iv_close, iv_back, iv_nav;
     EditText et_search;
 
     String URL = "", IP, API, PATH = "/", LAST_PHRASE="", CURRENT_PHRASE="";
-    Boolean STATUS_SEARCH = false;
+    Boolean STATUS_SEARCH = false, SEARCH_ACTIVE = false;
+    int INDEX_OBJECT;
+    long STORAGE_USED = 0;
+    long STORAGE_FREE = 0;
 
     List<String> objects = new ArrayList<String>(); // folders and files
     List<Integer> type_objects = new ArrayList<Integer>(); // 1=folder and 0=file
@@ -76,16 +74,25 @@ public class MainActivity extends AppCompatActivity {
         ImageView iv_logo = findViewById(R.id.iv_logo);
         AnimationImage(iv_logo);
 
-        ImageView btn_nav = findViewById(R.id.iv_nav);
-        btn_nav.setOnClickListener(f_nav);
+        iv_nav = findViewById(R.id.iv_nav);
+        iv_nav.setOnClickListener(f_nav);
 
         drawer_layout = findViewById(R.id.drawer_layout);
+
+        ll_no_objects = findViewById(R.id.ll_no_objects);
+        tv_info = findViewById(R.id.tv_info);
 
         iv_refresh = findViewById(R.id.iv_refresh);
         iv_refresh.setOnClickListener(f_refresh);
 
         iv_close = findViewById(R.id.iv_close_search);
         iv_close.setOnClickListener(f_close_search);
+//
+//        iv_back = findViewById(R.id.iv_back);
+//        iv_back.setOnClickListener(f_close_search);
+//
+////        iv_back = findViewById(R.id.iv_back);
+//        iv_back.setOnClickListener(f_close_search);
 
         tv_search = findViewById(R.id.tv_search);
         tv_search.setOnClickListener(f_search);
@@ -100,7 +107,15 @@ public class MainActivity extends AppCompatActivity {
         ll_main = findViewById(R.id.ll_main);
         pb_global = findViewById(R.id.pb_global);
 
+
+        /**
+         * drawer
+         */
+        tv_storage = drawer_layout.findViewById(R.id.tv_storage);
+        pb_storage = drawer_layout.findViewById(R.id.pb_storage);
+
         new ObjectsListing().execute();
+        new Storage().execute();
 
     }
 
@@ -115,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
             tv_search.setEnabled(false);
             ll_main.removeAllViews();
             pb_global.setVisibility(View.VISIBLE);
+            ll_no_objects.setVisibility(View.GONE);
 
             objects.clear();
             type_objects.clear();
@@ -168,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("result--", result);
 
 
+            ll_no_objects.setVisibility(View.GONE);
             if(result.equals("1")){
                 for (int i = 0; i < objects.size(); i++) {
                     if (type_objects.get(i) == 1) {
@@ -191,6 +208,8 @@ public class MainActivity extends AppCompatActivity {
                                 PATH += objects.get(index) + "/";
                                 tv_search.setVisibility(View.GONE);
                                 tv_root.setVisibility(View.VISIBLE);
+                                iv_nav.setVisibility(View.GONE);
+                                iv_back.setVisibility(View.VISIBLE);
                                 tv_root.setText(objects.get(index));
                                 new ObjectsListing().execute();
 //                                Intent intent_main_activity = new Intent(MainActivity.this, ListingActivity.class);
@@ -235,6 +254,11 @@ public class MainActivity extends AppCompatActivity {
                         TextView tv_details_object = ll_object.findViewById(R.id.tv_details_object);
                         tv_details_object.setText("Rozmiar: " + add_white_character(size_objects.get(i)));
                         ll_main.addView(ll_object);
+
+                        INDEX_OBJECT = i;
+
+
+                        iv_icon_action.setOnClickListener(f_more_action_file);
 
 //                        final int index = i;
 //                        iv_icon_action.setOnClickListener( new View.OnClickListener() {
@@ -346,6 +370,13 @@ public class MainActivity extends AppCompatActivity {
 //                        });
                     }
                 }
+
+
+
+                if(objects.size() == 0){
+                    ll_no_objects.setVisibility(View.VISIBLE);
+                    tv_info.setText(getResources().getString(R.string.main_no_objects));
+                }
             }
 
             ll_global.setVisibility(View.VISIBLE);
@@ -369,22 +400,10 @@ public class MainActivity extends AppCompatActivity {
                 STATUS_SEARCH = true;
                 new SearchObjectListing().execute();
             }
-//            ConstraintLayout ll_info = (ConstraintLayout) View.inflate(SearchActivity.this, R.layout.ll_info, null);
-//            TextView tv_info = ll_info.findViewById(R.id.tv_info);
-//            tv_info.setText("Trwa przeszukiwanie\nwedług frazy '"+current_word+"'");
-//            ll_main.addView(ll_info);
-//            if(!status_search){
-//                status_search = true;
-//                new SearchObjectListing().execute("http://"+ IP +"/search.php?key_api="+ API +"&&dir=/var/www/private_space/&&word="+current_word, current_word);
-//            }
-
-
         }
 
         @Override
-        public void afterTextChanged(Editable editable) {
-
-        }
+        public void afterTextChanged(Editable editable) {}
     };
 
     private class SearchObjectListing extends AsyncTask<String, Integer, String> {
@@ -449,15 +468,13 @@ public class MainActivity extends AppCompatActivity {
             Log.d("qwertyuiop", result);
             if(result.equals("1")) {
                 ll_main.removeAllViews();
+                ll_no_objects.setVisibility(View.GONE);
 
                 if(objects.size() == 0){
                     ll_main.removeAllViews();
-                    ConstraintLayout ll_info = (ConstraintLayout) View.inflate(MainActivity.this, R.layout.ll_search_info, null);
-                    TextView tv_info = ll_info.findViewById(R.id.tv_info);
-                    tv_info.setText("Brak wyników.");
-                    ll_main.addView(ll_info);
+                    ll_no_objects.setVisibility(View.VISIBLE);
                 }
-//
+
                 for (int i = 0; i < objects.size(); i++) {
                     if (type_objects.get(i) == 1) {
                         ConstraintLayout ll_object = (ConstraintLayout) View.inflate(MainActivity.this, R.layout.ll_object, null);
@@ -529,22 +546,99 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    private class Storage extends AsyncTask<String, Integer, String> {
+
+        OkHttpClient client = new OkHttpClient();
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String search_url = ("http://"+ IP +"/storage.php?key_api="+ API).replaceAll(" ", "%20");
+                Response response = client.newCall(new Request.Builder()
+                        .url(search_url)
+                        .build()).execute();
+                if (!response.isSuccessful()) {
+                    Log.d("executed-url-result", "null");
+                    return null;
+                }
+
+                JSONObject jA = new JSONObject(response.body().string());
+
+                if(jA.getInt("status") == 1){
+                    Log.d("qwertyuiop", jA.toString());
+                    STORAGE_USED = jA.getLong("storage_used");
+                    STORAGE_FREE = jA.getLong("storage_free");
+                    return "1";
+                }else{
+                    return "0";
+                }
+            } catch (UnknownHostException e) {
+                Log.d("status--ConnectExcep", e.toString());
+                return "-1";
+            } catch (ConnectException | SocketTimeoutException e){
+                Log.d("status--ConnectExcep", e.toString());
+                return "-2";
+            } catch (IllegalArgumentException e){
+                Log.d("status--ConnectExcep", e.toString());
+                return "-3";
+            } catch (Exception e){
+                Log.d("status--ConnectExcep", e.toString());
+                return "-4";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if(result.equals("1")){
+                Log.d("qwertyuiop", STORAGE_USED + "/" + STORAGE_FREE);
+
+                tv_storage.setText(humanReadableByteCountBin(STORAGE_USED) + " of " + humanReadableByteCountBin (STORAGE_FREE) + " (" + 100*STORAGE_USED/STORAGE_FREE + "% full)");
+                pb_storage.setProgress((int) (100*STORAGE_USED/STORAGE_FREE));
+
+            }
+
+
+        }
+    }
+
     @Override
     public void onBackPressed() {
-        if(!PATH.equals("/")){
-            String [] path = PATH.split("/");
-            PATH = TextUtils.join("/", Arrays.copyOf(path, path.length-1))+"/";
-            if(PATH.equals("/")){
-                tv_search.setVisibility(View.VISIBLE);
-                tv_root.setVisibility(View.GONE);
+        if(!SEARCH_ACTIVE){
+            if(!PATH.equals("/")){
+                String [] path = PATH.split("/");
+                PATH = TextUtils.join("/", Arrays.copyOf(path, path.length-1))+"/";
+                if(PATH.equals("/")){
+                    tv_search.setVisibility(View.VISIBLE);
+                    tv_root.setVisibility(View.GONE);
+                }else{
+                    tv_search.setVisibility(View.GONE);
+                    tv_root.setVisibility(View.VISIBLE);
+                    tv_root.setText(path[path.length-2]);
+                }
+                new ObjectsListing().execute();
             }else{
-                tv_search.setVisibility(View.GONE);
-                tv_root.setVisibility(View.VISIBLE);
-                tv_root.setText(path[path.length-2]);
+                this.finishAffinity();
             }
-            new ObjectsListing().execute();
         }else{
-            this.finishAffinity();
+            SEARCH_ACTIVE = false;
+            tv_search.setVisibility(View.VISIBLE);
+            tv_root.setVisibility(View.VISIBLE);
+            iv_refresh.setVisibility(View.VISIBLE);
+            et_search.setVisibility(View.GONE);
+            iv_close.setVisibility(View.GONE);
+            tv_root.setVisibility(View.GONE);
+            PATH = "/";
+            new ObjectsListing().execute();
+
         }
     }
 
@@ -562,6 +656,20 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    View.OnClickListener f_more_action_file = new View.OnClickListener() {
+        public void onClick(View v) {
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.popup_more_actions_file,
+                    (ViewGroup) findViewById(R.id.cl_popup_more_action));
+            final PopupWindow pw = new PopupWindow(layout, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, true);
+            pw.showAtLocation(layout, Gravity.BOTTOM, 0, 0);
+
+            TextView iv_name = layout.findViewById(R.id.tv_name);
+            iv_name.setText(objects.get(INDEX_OBJECT));
+        }
+    };
+
     View.OnClickListener f_search = new View.OnClickListener() {
         public void onClick(View v) {
             et_search.setVisibility(View.VISIBLE);
@@ -571,6 +679,7 @@ public class MainActivity extends AppCompatActivity {
             iv_refresh.setVisibility(View.GONE);
             tv_root.setVisibility(View.GONE);
             ll_main.removeAllViews();
+            SEARCH_ACTIVE = true;
         }
     };
 
@@ -583,13 +692,14 @@ public class MainActivity extends AppCompatActivity {
             iv_close.setVisibility(View.GONE);
             tv_root.setVisibility(View.GONE);
             PATH = "/";
+            SEARCH_ACTIVE = false;
             new ObjectsListing().execute();
         }
     };
 
     View.OnClickListener f_refresh = new View.OnClickListener() {
         public void onClick(View v) {
-            Log.d("assadsdadsa", "1234567890");
+            new ObjectsListing().execute();
         }
     };
 
@@ -600,5 +710,20 @@ public class MainActivity extends AppCompatActivity {
             __result.append(" ");
         }
         return __result + __str;
+    }
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %cB", value / 1024.0, ci.current());
     }
 }
